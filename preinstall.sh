b9adad8f96ae534d9c8729038dd19eff9fc2eea0
Alex Forrence <alex.forrence@gmail.com>
2020-09-03 17:55:32 -0400

    update build steps

--- preinstall.sh
+++ preinstall.sh
@@ -1,5 +1,3 @@
 # preinstall things that aren't on pypi proper
-pip install -f https://github.com/aforren1/ptb-wheels/releases psychtoolbox
-pip install git+https://github.com/swistakm/pyimgui
 pip install ./mglg
-pip install pyinstaller
+pip install pyinstaller==3.6
